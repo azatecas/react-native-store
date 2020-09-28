43de4db2d1309db6db6e9a54eabd7b1ce2f3a50c
Luis PM <lpm@pop-os.localdomain>
2020-09-28 06:29:06 -0400

    initialized ErrorMessage component

---
+++ app/components/ErrorMessage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View, Text, StyleSheet} from 'react-native'
+import AppText from './AppText';
+
+const ErrorMessage = ({ error }) => {
+    if (!error) return null;
+
+    return (
+        <AppText style={styles.error}>
+            {error}
+        </AppText>
+    )
+}
+
+const styles = StyleSheet.create({
+    error: {
+        color: "red"
+    }
+})
+
+
+
+export default ErrorMessage
